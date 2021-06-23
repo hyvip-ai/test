bee7437235a280a9b5c11d1aed2f21d39246e65a
hyvip-ai <rm2932002@gmail.com>
2021-06-24 00:54:54 +0530

    'second'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+    production: false,
+  firebase:{
+    apiKey: "AIzaSyAWZaB__kJLPtHWDNFi6EeZLgBoLlwWfTU",
+  authDomain: "upjaoconsumer-1f03d.firebaseapp.com",
+  databaseURL: "https://upjaoconsumer-1f03d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "upjaoconsumer-1f03d",
+  storageBucket: "upjaoconsumer-1f03d.appspot.com",
+  messagingSenderId: "692810550957",
+  appId: "1:692810550957:web:8fa0d407b4fa773681c024"
+  }
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAWZaB__kJLPtHWDNFi6EeZLgBoLlwWfTU",
+  authDomain: "upjaoconsumer-1f03d.firebaseapp.com",
+  databaseURL: "https://upjaoconsumer-1f03d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "upjaoconsumer-1f03d",
+  storageBucket: "upjaoconsumer-1f03d.appspot.com",
+  messagingSenderId: "692810550957",
+  appId: "1:692810550957:web:8fa0d407b4fa773681c024"
+  }
 };
 
 /*
